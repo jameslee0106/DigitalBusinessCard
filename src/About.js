9408ade63b51b342167f6d1d7a8d7d353e103c4a
jameslee0106 <leejames0106@gmail.com>
2023-01-17 18:13:44 -0800

    updating about me and interests

--- src/About.js
+++ src/About.js
@@ -6,7 +6,7 @@ export default function About() {
     <div className="About">
       <h2>About</h2>
       <p>
-        I am a fullstack developer with an interest in learning and applying new technologies. I am currently studying the MERN stack and working towards applying it to projects that align with my hobbies. 
+        I am a fullstack developer with an interest in learning and diving into new topics. I am currently studying the MERN stack. During my free time, I plan to work towards applying my knowledge to projects that align with my hobbies. 
       </p>
     </div>
   )
--- src/Interests.js
+++ src/Interests.js
@@ -5,7 +5,7 @@ export default function Interests() {
   return (
     <div className="Interests">
       <h2>Interests</h2>
-      <p>Huge fan of the Seattle Seahawks and Mariners. On my free time, I enjoy playing soccer, volleyball, and bouldering. Tech enthusiast! </p>
+      <p>Huge fan of the Seattle Seahawks and Mariners. I enjoy playing soccer, volleyball, and bouldering. Tech enthusiast! </p>
     </div>
   )
 }
